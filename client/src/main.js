719887cbffb3553e4f41378760f7a5f41c2d672e
Wenqiang Li <wenqiangli@WenqiangtekiMacBook-Pro.local>
2017-12-23 21:39:33 +0800

    misc

---
+++ client/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import FastClick from 'fastclick'
+import VueRouter from 'vue-router'
+import App from './App'
+import Home from './components/HelloFromVux'
+
+Vue.use(VueRouter)
+
+const routes = [{
+  path: '/',
+  component: Home
+}]
+
+const router = new VueRouter({
+  routes
+})
+
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app-box')
